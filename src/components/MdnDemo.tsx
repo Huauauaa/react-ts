a63d44013b9c5a9465f13ff55bc1debe047bc683
Harvey <harvey0379@163.com>
2021-02-16 09:15:06 +0800

    ✨ Map

--- src/components/MdnDemo.tsx
+++ src/components/MdnDemo.tsx
@@ -5,12 +5,38 @@ export default function MdnDemo() {
   const numbers: Array<number> = [1, 30, 4, 21, 100000];
   const sortedNumbers: Array<number> = [...numbers].sort();
 
+  const fooMap = new Map();
+  fooMap.set(0, '0');
+  fooMap.set(-0, '-0');
+  fooMap.set(+0, '+0');
+  fooMap.set(NaN, 'NaN');
+  fooMap.set(NaN, 'not a number');
+
+  console.log(fooMap);
+
   return (
-    <ul>
-      <ol>months: {months.join(' ')}</ol>
-      <ol>months.sort(): {sortedMonths.join(' ')}</ol>
-      <ol>numbers: {numbers.join(' ')}</ol>
-      <ol>numbers.sort(): {sortedNumbers.join(' ')}</ol>
-    </ul>
+    <>
+      <fieldset>
+        <legend>Map</legend>
+        <ul>
+          {Array.from(fooMap.keys()).map((key: any, index: number) => {
+            return (
+              <li key={index}>
+                {key}: {fooMap.get(key)}
+              </li>
+            );
+          })}
+        </ul>
+      </fieldset>
+      <fieldset>
+        <legend>sort</legend>
+        <ul>
+          <ol>months: {months.join(' ')}</ol>
+          <ol>months.sort(): {sortedMonths.join(' ')}</ol>
+          <ol>numbers: {numbers.join(' ')}</ol>
+          <ol>numbers.sort(): {sortedNumbers.join(' ')}</ol>
+        </ul>
+      </fieldset>
+    </>
   );
 }
