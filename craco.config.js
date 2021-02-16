d0dd5d130a64d0a8bc8213fcf7f8312fccbdf41f
Harvey <harvey0379@163.com>
2021-02-16 09:14:38 +0800

    🔧 add version info

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,20 @@
 const CracoLessPlugin = require('craco-less');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin();
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      const htmlWebpackPluginInstance = webpackConfig.plugins.find(
+        (webpackPlugin) => webpackPlugin instanceof HtmlWebpackPlugin,
+      );
+      htmlWebpackPluginInstance.options.version = gitRevisionPlugin.version();
+      htmlWebpackPluginInstance.options.branch = gitRevisionPlugin.branch();
+      htmlWebpackPluginInstance.options.commitHash = gitRevisionPlugin.commithash();
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
