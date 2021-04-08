148139f0da61593a425b9688fecccaa2c1db5b27
huaweiwei <harvey0379@163.com>
2021-04-08 14:05:34 +0800

    test: destroy component

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+
 function Child(props: any) {
+  const [input, setInput] = useState('please input');
+  const close = () => {
+    props.onclose();
+  };
+
   return (
     <div className="child">
+      <input type="text" value={input} onInput={(e: any) => setInput(e.target.value)} />
+      <button onClick={close}>close</button>
       {props?.info?.text}
       {props?.name}
     </div>
--- src/components/ECMA.tsx
+++ src/components/ECMA.tsx
@@ -1,14 +1,17 @@
 import Child from './Child';
 import '../assets/styles/ecma.less';
+import { useState } from 'react';
 
 function ECMA() {
   const info = {
     id: 1,
   };
+  const [visible, setVisible] = useState(true);
   return (
     <fieldset className="ecma">
       <legend>ECMA</legend>
-      <Child name="hi" info={info}></Child>
+      <button onClick={() => setVisible(true)}>open</button>
+      {visible && <Child name="hi" info={info} onclose={() => setVisible(false)}></Child>}
       <ul>
         <li>1</li>
         <li>2</li>
