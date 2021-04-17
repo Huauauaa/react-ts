b5aa4b6744e5f4f8a66a2c53a12411b4671dd1a0
huaweiwei <harvey0379@163.com>
2021-04-17 19:04:20 +0800

    add proxy

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const About = () => {
+  useEffect(() => {
+    console.log('111');
+    (async () => {
+      const resp = await axios.get('/api/users');
+      console.log(resp);
+    })();
+  }, []);
+
   return <div>About</div>;
 };
 
